dedfb2d961ee329a75c4cfec95d09fd39483750c
Maria Titova <mary.titova.1995@gmail.com>
2021-02-07 21:59:35 +0300

    fix: webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,12 +10,7 @@ module.exports = {
   entry: './index.js',
   output: {
     filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, './dist'),
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000,
+    path: path.resolve(__dirname, './dist/src'),
   },
   plugins: [
     new HTMLWebpackPlugin({
